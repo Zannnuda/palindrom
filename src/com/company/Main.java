f6d65e91d73332efb2a728a2d9c7c4b792576596
Zannnuda <85441242+Zannnuda@users.noreply.github.com>
2021-10-15 15:19:01 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        String str = "madam";
+//        System.out.println(str.equals(new StringBuilder(str).reverse().toString()));
+        
+
+    }
+}
